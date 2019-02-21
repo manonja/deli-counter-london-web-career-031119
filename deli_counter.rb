16d10252004b1743f4080c218aed858041712edd
manon jacquin <manon.jacquin@gmail.com>
2019-02-21 08:59:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+
+def line(people)
+  list = []
+    if people.length == 0
+      puts "The line is currently empty."
+      
+    else
+    people.each.with_index(1) do |name, index| 
+      list.push("#{index}. #{name}")
+    end
+  end
+  puts "The line is currently: #{list.join(" ")}."
+end
